7445176b2397529dcc1fda45db7e973909aa5286
Cameron Blackwood <cameronjwblackwood@gmail.com>
2020-03-28 12:18:17 +0000

    athing class selctor now working

--- scraper.py
+++ scraper.py
@@ -14,10 +14,15 @@ src = result.content
 soup = BeautifulSoup(src, 'lxml')
 
 links = soup.find_all("a")
+stuff = soup.find_all('div', {"class": "athing"})
 urls = []
 for link in soup.find_all("a"):
     site = link.get('href')
     if "http" in site:
         urls.append(site)
+athings = []
+for items in soup.find_all('tr', {"class": "athing"}): 
+    athings.append(items)
 print(urls)
 print(len(urls))
+print(athings)
